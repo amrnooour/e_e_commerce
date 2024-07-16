9a88540982877ec27a3f995f7e586f23a6a010cd
Your Name <you@example.com>
2024-07-16 12:23:33 +0300

    add images and fonts

---
+++ lib/features/onboarding/presentation/views/onboarding_view.dart
@@ -0,0 +1,13 @@
+import 'package:e_commerce/features/onboarding/presentation/views/widgets/onboarding_view_body.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingView extends StatelessWidget {
+  const OnboardingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: OnboardingViewBody(),
+    );
+  }
+}
